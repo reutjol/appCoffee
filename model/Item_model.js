e3bd38137bcbc1c8e133416e414c4d18c868826c
Reut Uzan <101014327+reutjol@users.noreply.github.com>
2024-03-27 10:35:19 +0200

    Update Item_model.js

--- model/Item_model.js
+++ model/Item_model.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    img:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true,
-        default:0
-    },
-    rate:{
-        type:String,
-        default:2
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  rate: {
+    type: String,
+    default: 2,
+  },
+});
 
-module.exports = Item = mongoose.model('item',ItemSchema);
+module.exports = Item = mongoose.model("item", ItemSchema);
