6aa0dddec8cf0893b4f27a9e1d84fcd1421b3bf0
Reut Uzan <101014327+reutjol@users.noreply.github.com>
2024-03-27 10:35:46 +0200

    Update Order_model.js

--- model/Order_model.js
+++ model/Order_model.js
@@ -2,38 +2,37 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-  user: {
+const PreferencesSchema = new Schema({
+  item: {
     type: Schema.Types.ObjectId,
-    ref: "users",
+    ref: "items",
     required: true,
   },
-  preferences: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "preferences",
-      required: true,
-    },
-  ],
-  orderTotalQuantity: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  orderTotalAmount: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  status: {
+  milk: {
     type: String,
     required: true,
-    default: "new",
+    default: "Ragular",
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  size: {
+    type: String,
+    required: true,
+    default: "small",
+  },
+  remarks: {
+    type: String,
+    required: true,
+    default: " ",
+  },
+  is_hot: {
+    type: Boolean,
+    required: true,
+    default: true,
   },
 });
 
-module.exports = Order = mongoose.model("order", OrderSchema);
+module.exports = Preferences = mongoose.model("preferences", PreferencesSchema);
