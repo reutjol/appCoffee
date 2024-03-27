bf66617407cbcb0bfa05cd363ea8dc9159fbdcc6
Reut Uzan <101014327+reutjol@users.noreply.github.com>
2024-03-27 10:34:12 +0200

    Update items.js

--- routes/api/items.js
+++ routes/api/items.js
@@ -19,77 +19,10 @@ router.get("/:id", (req, res) => {
     .catch((err) => res.status(404).json({ success: false }));
 });
 
-//Get category Items
-//no need because we deleted category
-// router.get("/category/:category", (req, res) => {
-//   // console.log("cat id",req.params.category);
-//   Item.find({ category: { $regex: req.params.category, $options: "i" } })
-//     .then((item) => res.json(item))
-//     .catch((err) => res.status(404).json({ success: false }));
-// });
-
 // Get named items
 router.get("/name/:name", (req, res) => {
   Item.find({ name: { $regex: req.params.name, $options: "i" } })
     .then((item) => res.json(item))
-    .catch((err) => res.status(404).json({ success: false }));
-});
-
-//Get named items within category
-//no need because we deleted category
-// router.get("/category/:category/name/:name", (req, res) => {
-//   // console.log("cat id",req.params.category);
-//   // console.log("name id",req.params.name);
-//   Item.find({
-//     $and: [
-//       { category: req.params.category },
-//       { name: { $regex: req.params.name, $options: "i" } },
-//     ],
-//   })
-//     .then((item) => res.json(item))
-//     .catch((err) => res.status(404).json({ success: false }));
-// });
-
-// Update specific Item
-// not sure if needed !
-router.put("/:id", auth, (req, res) => {
-  const updateItem = {
-    name: req.body.name,
-    description: req.body.description,
-    img: req.body.img,
-    img2: req.body.img2,
-    price: req.body.price,
-  };
-  // console.log(updateItem);
-  Item.findByIdAndUpdate({ _id: req.params.id }, updateItem, (err, update) => {
-    if (err) res.status(500).send();
-    else res.send(update);
-  });
-});
-
-// Add an Item
-// not sure if needed !
-
-router.post("/", auth, (req, res) => {
-  const newItem = new Item({
-    name: req.body.name,
-    description: req.body.description,
-    img: req.body.img,
-    img2: req.body.img2,
-    price: req.body.price,
-  });
-  newItem
-    .save()
-    .then((item) => res.json(item))
-    .catch((err) => res.status(404).send(err));
-});
-
-// Hard Delete an Item
-// not sure if needed !
-
-router.delete("/:id", auth, (req, res) => {
-  Item.findByIdAndRemove(req.params.id)
-    .then(() => res.json({ success: true }))
     .catch((err) => res.status(404).json({ success: false }));
 });
 
