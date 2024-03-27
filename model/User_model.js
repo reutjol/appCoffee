fbbbfa9ded1cf1624eaef8be8b5c13aba7578f94
Reut Uzan <101014327+reutjol@users.noreply.github.com>
2024-03-27 10:36:13 +0200

    Update User_model.js

--- model/User_model.js
+++ model/User_model.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-   isAdmin: {
-        type: Boolean,
-        default: false 
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    register_date:{
-        type:Date,
-        default:Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = User = mongoose.model('user',UserSchema);
+module.exports = User = mongoose.model("user", UserSchema);
